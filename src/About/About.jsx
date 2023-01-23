968ea43cf272703e8b2d377a9c12ef6842464fae
sowjanya-dhanekula <dhanekula.sowjanya@gmail.com>
2023-01-23 23:12:54 +0530

    checking-in first branch

---
+++ src/About/About.jsx
@@ -0,0 +1,12 @@
+import "./About.css";
+import React from "react";
+
+function template() {
+  return (
+    <div className="about">
+      <h3>This is my About page</h3>
+    </div>
+  );
+};
+
+export default template;
---
+++ src/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div class='text-center bg-primary text-white header'>My First React Application</div>
+  )
+}
+
+export default Header
